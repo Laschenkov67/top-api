f8e4ed719cdbe3283456d71e7b8d41062dced1f5
Artem Laschenkov <laschenkov59@yandex.ru>
2021-10-31 20:44:09 +0300

    feat: Подключение к Mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,28 @@
+import { getMongoConfig } from './configs/mongo.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ReviewModule } from './review/review.module';
+import { TypegooseModule } from 'nestjs-typegoose';
+
 
 @Module({
   imports: [
     AuthModule, 
+    ConfigModule.forRoot(),
     ProductModule, 
     ReviewModule, 
-    TopPageModule
+    TopPageModule,
+    TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig
+		}),
   
   ],
   controllers: [AppController],
