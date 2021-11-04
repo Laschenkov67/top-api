52b3d69279a2791020e401bfed4050e4c0a6111f
Artem Laschenkov <laschenkov59@yandex.ru>
2021-11-04 23:25:50 +0300

    FEAT | Сервис продуктов и агрегация

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,6 +2,7 @@ import { ProductModel } from './product.model';
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { ProductService } from './product.service';
 
 @Module({
   controllers: [ProductController],
@@ -14,6 +15,7 @@ import { TypegooseModule } from 'nestjs-typegoose';
 				}
 			}
 		])
-	]
+	],
+	providers: [ProductService]
 })
 export class ProductModule {}
